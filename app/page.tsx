42538a03e92dabafe32021b0156d72ce9bf915aa
leelongxi <leelongxi@foxmail.com>
2024-10-22 16:20:00 +0800

    feat: finish

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
  * @Author: leelongxi leelongxi@foxmail.com
  * @Date: 2024-10-22 15:05:26
  * @LastEditors: leelongxi leelongxi@foxmail.com
- * @LastEditTime: 2024-10-22 16:17:22
+ * @LastEditTime: 2024-10-22 16:19:07
  * @FilePath: /openspace_web3modal/app/page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -40,6 +40,9 @@ function ReactNFT() {
   if (ownerError || tokenURIError) return <div>Failed to fetch data</div>;
   return (
     <div>
+      <h3>
+        NFT:
+      </h3>
       <p>Owner of token: {owner as string}</p>
       <p>Metadata URI of token: {tokenURI as string}</p>
     </div>
